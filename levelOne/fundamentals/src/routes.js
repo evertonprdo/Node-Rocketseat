a16a2389c1d90437e3d6c8bfa27390e37fbb8fa7
Everton Cezar do Prado Junior <ecpj.prado@gmail.com>
2024-08-27 12:25:24 -0300

    Starting to develop business rules
    Começando a desenvolver a lógica de negócio da aplicação

---
+++ levelOne/fundamentals/src/routes.js
@@ -0,0 +1,41 @@
+import { randomUUID } from 'node:crypto'
+import { Database } from "./database.js"
+import { buildRoutePath } from './utils/build-route-path.js'
+
+const database = new Database
+
+export const routes = [
+  {
+    method: 'GET',
+    path: buildRoutePath('/users'),
+    handler: (request, response) => {
+      const users = database.select('users')
+
+      return response.end(JSON.stringify(users))
+    }
+  },
+  {
+    method: 'GET',
+    path: buildRoutePath('/users'),
+    handler: (request, response) => {
+      const { name, email } = request.body
+
+      const user = {
+        id: randomUUID(),
+        name: name,
+        email: email
+      }
+
+      database.insert('users', user)
+
+      return response.writeHead(201).end()
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (request, response) => {
+      response.end()
+    }
+  }
+]
